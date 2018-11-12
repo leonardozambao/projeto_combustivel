e725d84bf9d8227acc80fc9a2d9c26fdc8853879
Leonardo Zambão <37703983+leonardozambao@users.noreply.github.com>
2018-11-12 14:22:28 -0200

    Add files via upload

---
+++ sqlScript.sql
@@ -0,0 +1,544 @@
+CREATE TABLE Carro(
+	nome varchar(120), 
+	kl_cidadeGas decimal(10,2),
+	kl_estradaGas decimal(10,2),
+	kl_cidadeAlc decimal(10,2),
+	kl_estradaAlc decimal(10,2)
+);
+
+CREATE TABLE Moto(
+	nome varchar(120), 
+	kl_cidadeGas decimal(10,2),
+	kl_estradaGas decimal(10,2),
+	kl_cidadeAlc decimal(10,2),
+	kl_estradaAlc decimal(10,2)
+);
+
+
+/* inserts carros */
+
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Renault Kwid', 
+	14.9, 
+	15.6, 
+	10.3, 
+	10.8
+);
+
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Toyota Corolla', 
+	11.4, 
+	13.2, 
+	7.8, 
+	9.2
+);
+
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Fiat Toro', 
+	8.6, 
+	10.8, 
+	5.9, 
+	7.4
+);
+
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Honda HR-V', 
+	11, 
+	12.3, 
+	7.7, 
+	8.6
+);
+
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Hyundai Creta', 
+	8.2, 
+	11.4, 
+	6.9, 
+	10
+);
+
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Fiat Mobi', 
+	9.2, 
+	13.3, 
+	8.4, 
+	11.9
+);
+
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Nissan Kicks', 
+	11.4, 
+	13.4, 
+	7.7, 
+	9.4
+);
+
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Volks. Voyage', 
+	11.1, 
+	13.3, 
+	7.5, 
+	10.1
+);
+
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Volks. Virtus', 
+	11.2, 
+	14.6, 
+	7.8, 
+	10.2
+);
+
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Fiat Uno', 
+	13.2, 
+	13.7, 
+	9.3, 
+	10.1
+);
+
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Onix', 
+	12.9, 
+	15.3, 
+	9.1, 
+	10.8
+);
+
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Ford Ka', 
+	8.9, 
+	15.3, 
+	13.1, 
+	15.8
+);
+
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'HB20', 
+	7.1, 
+	15.3, 
+	7.1, 
+	9.4
+);
+
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Gol', 
+	11.1, 
+	9.3, 
+	7.7, 
+	13.3
+);
+
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Fiat Argo', 
+	14.2, 
+	15.1, 
+	9.9, 
+	10.7
+);
+
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Sandero', 
+	12.2, 
+	14.3, 
+	8.8, 
+	10.8
+);
+
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Jeep Compass', 
+	8.1, 
+	10.5, 
+	5.5, 
+	7.2
+);
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Polo', 
+	12.9, 
+	14.3, 
+	8.8, 
+	10
+);
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Fiat Strada', 
+	7.5, 
+	10.3, 
+	5.1, 
+	7.8
+);
+insert into Carro 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Prisma', 
+	12.5, 
+	15.7, 
+	9.5, 
+	11.8
+);
+/* inserts motos */
+
+
+insert into Moto 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Fan 160', 
+	31, 
+	35, 
+	25, 
+	28
+);
+
+insert into Moto 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Honda Biz', 
+	41, 
+	45, 
+	36, 
+	39
+);
+
+insert into Moto 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'CB Twister', 
+	26, 
+	30, 
+	25, 
+	28
+);
+
+insert into Moto 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Yamaha crosser', 
+	35, 
+	38, 
+	27, 
+	29
+);
+
+insert into Moto 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Hornet cb 600', 
+	24, 
+	23, 
+	23, 
+	26
+);
+
+insert into Moto 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Suzuki v strom', 
+	19, 
+	23, 
+	16, 
+	18
+);
+
+insert into Moto 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Honda pcx', 
+	26, 
+	30, 
+	22, 
+	25
+);
+
+insert into Moto 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Honda pop 110', 
+	38, 
+	41, 
+	33, 
+	35
+);
+
+insert into Moto 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'BMW s1000', 
+	14.38, 
+	16, 
+	11.5, 
+	13.2
+);
+
+insert into Moto 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Yamaha xzt 250', 
+	27, 
+	30, 
+	25, 
+	28
+);
+
+insert into Moto 
+(
+	nome,
+	kl_cidadeGas,
+	kl_estradaGas,
+	kl_cidadeAlc,
+	kl_estradaAlc
+)
+values 
+(  
+	'Honda Bros 160', 
+	30, 
+	36, 
+	21, 
+	25
+);
